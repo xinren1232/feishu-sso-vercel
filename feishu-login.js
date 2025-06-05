e76c3a15a99dd4233bea548fdd3887cdfa07e68f
New 0→1 <543927692@qq.com>
2025-06-05 10:03:05 +0800

    Add files via upload

---
+++ feishu-login.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  const { code } = req.body;
+  const appId = process.env.FEISHU_APP_ID;
+  const appSecret = process.env.FEISHU_APP_SECRET;
+
+  const tokenResp = await fetch('https://open.feishu.cn/open-apis/authen/v1/access_token', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + Buffer.from(`${appId}:${appSecret}`).toString('base64'),
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ grant_type: 'authorization_code', code })
+  });
+
+  const tokenData = await tokenResp.json();
+  const accessToken = tokenData.data?.access_token;
+
+  const userResp = await fetch('https://open.feishu.cn/open-apis/authen/v1/user_info', {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`
+    }
+  });
+
+  const userInfo = await userResp.json();
+  res.status(200).json(userInfo.data);
+}
